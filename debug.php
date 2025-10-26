a46ab9a4b44bb43c45aee338258193e9407134db
ivan <iscillone@gmail.com>
2025-10-26 18:34:13 +0100

    Finito il Debugger

--- debug.php
+++ debug.php
@@ -1,12 +1,10 @@
 <?php
 class Debug
 {
-    /**
-     * File (path incluso) in cui scrivere il Log.
-     * 
-     * @var string
-     */
-    private $log;
+    /** @var string File (path incluso) in cui scrivere il Log */
+    private $logFile;
+    /** @var string Classe del tag PRE contenente il dump di una variabile */
+    private $class;
 
     /**
      * Costruttore.
@@ -14,25 +12,41 @@ class Debug
      * Crea l'oggetto Debug e, se non altrimenti specificato, scrive il Log 
      * nella directory dello script chiamante, usando il nome "debug.log".
      * 
-     * @param string $log Path e nome del file in cui scrivere il Log.
+     * @param string $logFile Path e nome del file in cui scrivere il Log.
+     * @param string $class Classe da usare per varDump.
      */
-    public function __construct(string $log='')
+    public function __construct(string $logFile='', string $class='')
     {
         $trace=debug_backtrace();
 
-        if ($log=='') {
-            $this->log=dirname($trace[0]['file']).'/debug.log';
+        if ($logFile=='') {
+            $this->logFile=dirname($trace[0]['file']).'/debug.log';
         } else {
-            $this->log=$log;
+            $this->logFile=$logFile;
         }
+        $this->class=$class;
     }
+    /**
+     * Legge la proprietà logFile
+     * 
+     * @return string 
+     */
+    public function getFile(): string
+    {
+        return $this->logFile;
+    }
+    /**
+     * Registra nel Log un'informazione.
+     * 
+     * @param mixed $x Il dato da registrare
+     */
     public function log(mixed $x): void
     {
         $now=new DateTimeImmutable();
         $trace=debug_backtrace();
-        $f=fopen($this->log,'a');
-        fwrite($f,$now->format('❬d M, H:i:s.u❭ '));
-        fprintf($f,'❬%s:%d❭ ',basename($trace[0]['file']),$trace[0]['line']);
+        $f=fopen($this->logFile,'a');
+        fwrite($f,$now->format('⟨d M, H:i:s.u⟩ '));
+        fprintf($f,'⟨%s:%d⟩ ',basename($trace[0]['file']),$trace[0]['line']);
         if (is_scalar($x) && !is_bool($x)) {
             fwrite($f,$x);
         } else {
@@ -40,5 +54,65 @@ class Debug
         }
         fwrite($f,PHP_EOL);
         fclose($f);
+    }
+    /**
+     * Miglioramento di var_dump().
+     * 
+     * @param array $vars Le variabili di cui fare il dumping.
+     */
+    public function dump(...$vars): void
+    {
+        $trace=debug_backtrace();
+        echo "<pre class='{$this->class}'>⟨".basename($trace[0]['file']).':'
+            .$trace[0]['line']."⟩\n";
+        foreach ($vars as $v) {
+            echo '⟨'.gettype($v).'⟩ ';
+            if (is_scalar($v)) {
+                echo json_encode($v,JSON_UNESCAPED_SLASHES);
+            } else {
+                echo json_encode($v,JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT);
+            }
+            echo PHP_EOL;
+        }
+        echo "</pre>\n";
+    }
+    /**
+     * Ispeziona un oggetto.
+     * 
+     * Ispeziona l'oggetto fornito come argomento,
+     * se $view è True visualizza il risultato,
+     * altrimenti lo restituisce
+     * 
+     * @param object $x L'oggetto da ispezionare.
+     * @param bool $view Se True visualizza l'output.
+     * 
+     * @return string
+     */
+    public function inspect(object $x, bool $view=true): string
+    {
+        $ref=new ReflectionObject($x);
+        $buf='Class: '.$ref->getName().PHP_EOL;
+        $buf.='Properties: ';
+        $list=$ref->getProperties();
+        if (!$list) {
+            $buf.='⟨None⟩';
+        } else {
+            foreach ($list as $item) {
+                $buf.=$item->getName().', ';
+            }
+        }
+        $buf=rtrim($buf,', ').PHP_EOL;
+        $buf.='Methods: ';
+        $list=$ref->getMethods();
+        if (!$list) {
+            $buf.='⟨None⟩';
+        } else {
+            foreach ($list as $item) {
+                $buf.=$item->getName().', ';
+            }
+        }
+        $buf=rtrim($buf,', ').PHP_EOL;
+        if ($view) echo "<pre class='{$this->class}'>{$buf}</pre>\n";
+        return $buf;
     }
 }
--- index.php
+++ index.php
@@ -5,14 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Test libreria</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <style>
+    .dbg { 
+        color: #00ff00; 
+        background: black;
+        padding: 0 4px;
+    }
+    </style>
 </head>
 <body>
 <?php
 require 'form.php';
 require 'debug.php';
 
-$form=new Form($_REQUEST);
-$debug=new Debug();
+$form=new Form($_REQUEST,$_POST,$_GET);
+$debug=new Debug(class: 'dbg');
 ?>
 <div class="container">
     <form method="post">
@@ -23,8 +30,12 @@ $debug=new Debug();
         <p><button name="doIt" type="submit" class="btn btn-secondary">Engage!</button></p>
     </form>
 <?php
-echo '<pre>Dati inviati → '.json_encode($_REQUEST,JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT)."</pre>\n";
 $debug->log($_POST);
+$obj=new Exception();
+$debug->dump($_POST,true,1701,$obj);
+$debug->inspect($obj);
+$obj=(object)['name'=>'Sherlock','surname'=>'Holmes'];
+$debug->log($debug->inspect($obj,false));
 ?>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
