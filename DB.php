0e5105f41b761b3ac5c8a0c023309052df6d9ad6
ivan <iscillone@gmail.com>
2025-11-11 12:48:45 +0100

    Eliminato il metodo fromArray()

--- DB.php
+++ DB.php
@@ -6,10 +6,10 @@ class DB extends \PDO
     /**
      * Crea l'oggetto DB
      * 
-     * Crea un oggetto usando il costruttore padre, setta il DEFAULT_FETCH_MODE
-     * a FETCH_NUM ed estende la classe PDOStatement. Se $dsn è un array assume 
-     * che si tratti di un elenco di file SQLite, usa il primo per creare la
-     * connessione ed i successivi come "attachments"
+     * Crea l'oggetto, setta il DEFAULT_FETCH_MODE a FETCH_NUM e permette 
+     * l'estensione di PDOStatement. Se $dsn è un array assume che si tratti di 
+     * un elenco di file SQLite, usa il primo per creare la connessione ed i 
+     * successivi come "attachments"
      * 
      * @inheritDoc
      */
@@ -29,31 +29,6 @@ class DB extends \PDO
         }
         $this->setAttribute(SELF::ATTR_DEFAULT_FETCH_MODE,SELF::FETCH_NUM);
         $this->setAttribute(SELF::ATTR_STATEMENT_CLASS,['\MyClasses\DBStatement']);
-    }
-    /**
-     * Crea una connessione a più database
-     * 
-     * Dato un oggetto contente un serie di riferimenti a file SQLite,
-     * restituisce un connessione al primo DB della serie con "attaccati" i
-     * seguenti
-     * 
-     * @param array $x Array dei files SQLite.
-     * 
-     * @return object Connessione.
-     */
-    public static function fromArray(array $x)
-    {
-        foreach ($x as $i=>$item) {
-            if (!preg_match('/^(.+)\.(.*)$/i',basename($item),$found)) {
-                return false;
-            }
-            if ($i==0) {
-                $conn=new static('sqlite:'.$item);
-            } else {
-                $conn->exec("ATTACH DATABASE '{$item}' AS {$found[1]}");
-            }
-        }
-        return $conn;
     }
     /**
      * Converte una tabella in un array associativo.
