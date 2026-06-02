06fcfaa8f98be19e7a78c84edce9d7dce52a63e0
Ivan <ivan.scillone@gmail.com>
2026-06-02 19:06:29 +0200

    Documentata la classe Net, come da PSR-5

--- src/Net.php
+++ src/Net.php
@@ -1,13 +1,30 @@
 <?php
 namespace MyClasses;
-
+/**
+ * Estrae informazioni sulla rete.
+ */
 class Net
 {
+    /**
+     * Restituisce l'URL del server.
+     * 
+     * Restituisce protocollo + host + porta del server PHP.
+     * 
+     * @return string URL del server.
+     */
     public static function serverUrl(): string
     {
         return (($_SERVER['HTTPS'] ?? 'off') == 'on' ? 'https' : 'http').
                '://' . $_SERVER['HTTP_HOST'];
     }
+    /**
+     * Restituisce il path del server.
+     * 
+     * Aggiunge all'URL del server il path del file PHP in esecuzione, senza il 
+     * nome del file.
+     * 
+     * @return string Path del server.
+     */
     public static function pathOnServer(): string
     {
         return self::serverUrl().
